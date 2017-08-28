0d086e34b4b69e9c5b390229e2461f58a2a7c25b
Shayne Chow <zhouxc@live.com>
2017-08-28 19:05:31 +0800

    添加通知基类

---
+++ CSharp/Shayne.Common/Shayne.Common/DependencyObjectHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shayne.Common
+{
+    //public class DependencyObjectHelper
+    //{
+    //    public static ScrollViewer GetScrollViewer(DependencyObject depObj)
+    //    {
+    //        if (depObj is ScrollViewer)
+    //        {
+    //            return depObj as ScrollViewer;
+    //        }
+
+    //        for (int i = 0; i < Windows.UI.Xaml.Media.VisualTreeHelper.GetChildrenCount(depObj); i++)
+    //        {
+    //            var child = Windows.UI.Xaml.Media.VisualTreeHelper.GetChild(depObj, i);
+    //            var result = GetScrollViewer(child);
+    //            if (result != null) return result;
+    //        }
+    //        return null;
+    //    }
+    //}
+}
---
+++ CSharp/Shayne.Common/Shayne.Common/ObservableObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Shayne.Common
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            var handler = PropertyChanged;
+
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
